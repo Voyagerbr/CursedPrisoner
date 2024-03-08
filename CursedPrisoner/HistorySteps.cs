77511e208d4aded3fcbe5d87de79dd43ce27203e
Nicolas <nicolasdealmeidasoares71@gmail.com>
2024-03-08 09:12:48 -0300

    Commiting

---
+++ CursedPrisoner/HistorySteps.cs
@@ -0,0 +1,13 @@
+//public class HistorySteps 
+//{
+//    public int IdPage;
+  //  public string? Text;
+    //public bool HasAnswer = false;
+    //public string? Answer01;
+    //public string? Answer02;
+    //public int IdLevelAnswer01;
+    //public int IdLevelAnswer02;
+    //public bool HeroDied = false;
+
+    
+//}
