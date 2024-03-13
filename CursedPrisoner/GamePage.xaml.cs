858f50e17ff179f87ec575a271c875cc96a8c722
Nicolas <nicolasdealmeidasoares71@gmail.com>
2024-03-13 09:52:40 -0300

    Commit dia 13/03/2024

--- CursedPrisoner/GamePage.xaml.cs
+++ CursedPrisoner/GamePage.xaml.cs
@@ -7,23 +7,7 @@ public partial class GamePage : ContentPage
     public GamePage()
 	{
 		InitializeComponent();
-	
-	//history.Add(new HistorySteps()
-	//{
-		//IdPage="0",
-		//Text="Olá novamente Prisioneiro, você parece estar mais cansado do que da última vez, o que está esperando? Saia para a floresta rapidamente, você se lembrará de tudo ao longo do caminho",
-		//HasAnswer=false,
-	//});
-	//history.Add(new HistorySteps()
-	//{
-		//IdPage="1",
-		//Text="Após alguns minutos caminhando por aquela floresta úmida, escura e fedida, você encontra uma encruzilhada, que se separa em duas direções. Olhando o caminho à esquerda parece claro e leva a uma cidade, dizem que lá é bonito. Olhando à direita vê-se um bosque escuro e lá há uma pequena aldeia. Você pretende seguir qual caminho?",
-		//HasAnswer=true,
-		//Answer01="Esquerda",
-		//Answer02="Direita",
-		//IdLevelAnswer01="2",
-		//IdLevelAnswer02="3"
-	//});
 	}
+
     
 }
--- CursedPrisoner/HistorySteps.cs
+++ CursedPrisoner/HistorySteps.cs
@@ -1,13 +1,13 @@
-//public class HistorySteps 
-//{
-//    public int IdPage;
-  //  public string? Text;
-    //public bool HasAnswer = false;
-    //public string? Answer01;
-    //public string? Answer02;
-    //public int IdLevelAnswer01;
-    //public int IdLevelAnswer02;
-    //public bool HeroDied = false;
+public class HistorySteps
+{
+    public int IdPage;
+     public string? Texts;
+    public bool HasAnswer;
+    public string? Answer01;
+    public string? Answer02;
+    public int IdLevelAnswer01;
+    public int IdLevelAnswer02;
+    public bool HeroDied = false;
 
     
-//}
+}
