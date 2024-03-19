9fc10c5778458fc5a82f63c46610292978a57bb8
Nicolas <nicolasdealmeidasoares71@gmail.com>
2024-03-19 08:57:09 -0300

    Commit day 19/03

--- CursedPrisoner/GamePage.xaml.cs
+++ CursedPrisoner/GamePage.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.Maui.Controls;
+
 namespace CursedPrisoner;
 
 public partial class GamePage : ContentPage
@@ -31,7 +33,9 @@ public partial class GamePage : ContentPage
 		Texts="Após algumas horas andando, chegaste a Piddletown, as pessoas da cidade parecem já te reconhecer e oferecem alguns equipamentos. Em específico, uma lamparina e um cutelo.Qual você vai pegar",
 		HasAnswer=true,
 		Answer01="Lamparina",
-		Answer02="Cutelo"
+		Answer02="Cutelo",
+		IdLevelAnswer01=5,
+		IdLevelAnswer02=6
 	});
 	history.Add(new HistorySteps()
 		{
@@ -39,6 +43,59 @@ public partial class GamePage : ContentPage
 			Texts="Após alguns minutos andando, você se depara com uma criatura estranha que fala algumas palavras em uma língua estrangeira em seu ouvido, você tenta a atacar mas ela devia e te dá uma espada de presente e fala para fazer bom uso dela",
 			HasAnswer=false
 		});
+			history.Add(new HistorySteps()
+		{
+			IdPage=4,
+			Texts="Depois de algum tempo você chega a uma taverna, as pessoas parecem felizes em te ver, e perguntam se você se lembra delas. Você se lembra?",
+			HasAnswer=true,
+			Answer01="Sim",
+			Answer02="Não",
+		});
+			history.Add(new HistorySteps()
+		{
+			IdPage=5,
+			Texts="Saindo da cidade você entra em um campo aberto, sem árvores e nem pessoas. Repentinamente aparece um cachorro, como se houvesse saído do chão e te ataca, o que você vai fazer?",
+			HasAnswer=true,
+			Answer01="Atacar",
+			Answer02 = "Fugir",
+			IdLevelAnswer01=995,
+			IdLevelAnswer02=10
+
+		});
+		history.Add(new HistorySteps()
+		{
+			IdPage=6,
+			Texts="Saindo da cidade você entra em um campo aberto, sem árvores e nem pessoas. Repentinamente aparece um cachorro, como se houvesse saído do chão e te ataca, o que você vai fazer?",
+			HasAnswer=true,
+			Answer01="Atacar",
+			Answer02 = "Fugir"
+
+		});
+		history.Add(new HistorySteps()
+		{
+			IdPage=10,
+			Texts="Você conseguiu fugir, mas está cansado e com sede",
+			HasAnswer=false
+
+		});history.Add(new HistorySteps()
+		{
+			IdPage=11,
+			Texts="Você correu para o lado contrário da cidade e não tem nimguém por perto",
+			HeroDied=true
+
+		});
+		history.Add(new HistorySteps()
+	{
+		IdPage=995,
+		Texts = "Você tentou o atacar com  a lamparina, e a pele dele pegou fogo, porém , ele ainda conseguiu te devorar",
+		HeroDied = true
+	});
+		history.Add(new HistorySteps()
+	{
+		IdPage=998,
+		
+		HeroDied = true
+	});
 		Start();
 	}
  
@@ -69,6 +126,14 @@ public partial class GamePage : ContentPage
 			Button01.IsVisible = false;
       		Button02.IsVisible = false;
 		}
+		if(CurrentHistoryStep.HeroDied)
+		{
+			FrameGameOver.IsVisible = true;
+		}
+		else
+		{
+			FrameGameOver.IsVisible = false;
+		}
 	}
 	void NextButtonIsClicked(object sender, EventArgs args)
 	{
@@ -84,5 +149,9 @@ public partial class GamePage : ContentPage
   {
 	var IdButton02 = CurrentHistoryStep.IdLevelAnswer02;
     ChangeCurrentStoryStep(IdButton02);
+  }
+  void ButtonRetryIsClicked(object sender,EventArgs args)
+  {
+	Application.Current.MainPage = new MainPage();
   }
 }
